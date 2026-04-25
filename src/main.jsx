d3d65fe69e53aca0474862d16d71d3f4f0251a13
akashpatel200314 <akashpatel200314@gmail.com>
2026-04-24 21:03:48 -0500

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
-import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-createRoot(document.getElementById('root')).render(
-    <HashRouter>
-      <App />
-    </HashRouter>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <HashRouter>
+    <App />
+  </HashRouter>
 )
